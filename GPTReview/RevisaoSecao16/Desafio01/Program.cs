6221839658bd95149a952a586184783d9cdced29
Wilkson Pedrosa <wilkson.cc@gmail.com>
2025-04-10 07:49:17 -0300

    Começando uma nova revisão

---
+++ GPTReview/RevisaoSecao16/Desafio01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> nomes = new List<string>
+        {
+            "Ana", "Pedro", "Amanda", "Marcos", "Bianca", "Mário", "Alice"
+        };
+
+        Predicate<string> comecaComA = nome => nome.StartsWith("A");
+        var nomesComA = nomes.FindAll(comecaComA);
+        Console.WriteLine("Nomes que começam com A: ");
+        nomesComA.ForEach(Console.WriteLine);
+    }
+}
